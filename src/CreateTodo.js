cbccee2eb2c033a3338435eb5b8d73ae0404a25c
Dubuuuu <geraldquinto24@gmail.com>
2021-08-21 18:19:25 +0800

    CreateTodo and TodoList updated

--- src/CreateTodo.js
+++ src/CreateTodo.js
@@ -1,29 +1,31 @@
-import { data } from 'browserslist';
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
 
-export const CreateTodo= () => {
-    const {register, handleSubmit} = useForm()
+export const CreateTodo = () => {
+    const { register, handleSubmit } = useForm();
+    const history = useHistory()
 
     const onSubmit = handleSubmit((data) => {
-        alert(JSON.stringify(data))
-    })
+        alert(JSON.stringify(data));
+        history.push("/")
+    });
     return (
-    <div className="conatiner">
-        <div className="mt-3">
-            <h3>Create Todo import</h3>
-            <form>
-                <div className="form-group"> 
-                    <label htmlFor="text">Text:</label>
-                    <input className="form-control" ref={register} type="text" name="text" id="text" />
-                </div>
-                <div className="form-group">
-                    <button type="submit" className="btn btn-primary">
-                        Create Todo
-                    </button>
-                </div>
-            </form>
+        <div className="container">
+            <div className="mt-3">
+                <h3>Create Todo import</h3>
+                <form onSubmit={onSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="text">Text:</label>
+                        <input className="form-control" {...register('text')} type="text" name="text" id="text" />
+                    </div>
+                    <div className="form-group">
+                        <button type="submit" className="btn btn-primary">
+                            Create Todo
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
-    </div>
     )
 }
